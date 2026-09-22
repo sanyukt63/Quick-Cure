a7e129b3d26e0e7eb1465ac3236c517e502f88a7
Sanyukt Kumar Rai <sanyuktkumarrai@gmail.com>
2026-09-22 23:40:17 +0530

    Fix token check logic in auth middleware

--- QuickCure/QuickCure-main/Backend/middlewares/authMiddleware.js
+++ QuickCure/QuickCure-main/Backend/middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
-  }
+  } 
 
   const isBlackListed = await blacklistTokenModel.findOne({ token: token });
   if (isBlackListed) {
